55ab8c0b979d6bba2f2340a1e3869382b5b6fb9c
ADARSH KUMAR PATEL <49089062+iadarshpatel@users.noreply.github.com>
2025-07-20 14:09:53 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const { prisma } = require("../db/config");
 
 async function createUserWithPost({ name, email, title, content }) {
-  // Write your code here
+  try {
+    await prisma.$transaction(async (tx) => {
+      const user = await tx.user.create({
+        data: { name, email }
+      });
+      await tx.post.create({
+        data: {
+          title,
+          content,
+          authorId: user.id
+        }
+      });
+    });
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
 }
 
 module.exports = { createUserWithPost };
